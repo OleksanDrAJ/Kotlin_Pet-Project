aa35e6d60ffa5ac4fa0ee30608e7027fe747b8d3
oleksandrdragan <drahan.o@ajax.systems>
2022-07-18 11:56:31 +0300

    Realised services and impl, started making controllers

--- src/main/kotlin/messanger/project/controller/ChatController.kt
+++ src/main/kotlin/messanger/project/controller/ChatController.kt
@@ -1,14 +1,33 @@
 package messanger.project.controller
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import messanger.project.model.Chat
+import messanger.project.model.User
 import messanger.project.service.ChatService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/api")
 class ChatController (
     @Autowired
     private val chatService: ChatService
 ){
+
+    @PostMapping("/chats-create")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createChat(@RequestBody users: HashSet<User>) {
+        chatService.createChat(users)
+    }
+
+    @PutMapping("/chats-add")
+    fun addUserToTheChat(@RequestBody user: User, chat: Chat) {
+        chatService.addUserToTheChat(chat, user)
+    }
+
+    @DeleteMapping("/chats/{name}")
+    fun delete(@PathVariable("name") name: Chat) {
+        chatService.deleteChat(name)
+    }
 
 }
--- src/main/kotlin/messanger/project/controller/UserController.kt
+++ src/main/kotlin/messanger/project/controller/UserController.kt
@@ -7,8 +7,9 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/user")
 class UserController(
-    @Autowired
+
     val userService: UserService
 ) {
 
